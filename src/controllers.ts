cb9f589b49840fcff2b4e6f75d726ac5dd55a5cd
Puriwat <uglypow@gmail.com>
2025-01-07 13:39:54 +0700

    feat: submit file

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,7 +1,22 @@
+import multer from "@koa/multer";
 import Router from "koa-router";
+
+export const upload = multer({ dest: "uploads/" });
 
 export const controller = new Router();
 
-controller.get("/hello", (ctx, next) => {
-  ctx.body = "Hello World!";
+controller.get("/status", (ctx) => {
+  ctx.body = { message: "OK" };
+});
+
+controller.post("/submit", upload.single("file"), async (ctx) => {
+  const file = ctx.request.file;
+
+  if (!file) {
+    ctx.status = 400;
+    ctx.body = { message: "No file uploaded" };
+    return;
+  }
+
+  ctx.body = { message: `File ${file.originalname} uploaded` };
 });
