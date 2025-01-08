2e24e007c220e2ea192dc8eab41b6479dc1dbb07
Puriwat <uglypow@gmail.com>
2025-01-08 13:48:51 +0700

    feat: mount submission container with volume

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,5 @@
 import { exec as origExec } from "child_process";
-import { join, resolve } from "path";
+import { join } from "path";
 import { promisify } from "util";
 import { extract } from "./utils/file_extractor";
 
@@ -11,22 +11,23 @@ export async function runTests(path: string, id: string) {
   const outputPath = join("work", id);
   await extract(path, outputPath);
 
-  const containerId = `submission-${id}`;
+  const containerName = `submission-${id}`;
+  const volumePath =  `/app/${id}`;
 
   try {
     await exec(`chmod -R 777 ${outputPath}`);
 
-    const command = `docker create --name ${containerId} --workdir /app --volume ${resolve(
-      outputPath
-    )}:/app my-openjdk tail -f /dev/null`;
+    // FIX: don't use hard code volume name "java-sandbox_submissions:/app"
+    // FIX: don't use hard code image name "my-openjdk" 
+    const command = `docker create --name ${containerName} --workdir /app --volume java-sandbox_submissions:/app my-openjdk tail -f /dev/null`;
 
     console.log(`Creating a container with '${command}'`);
     await exec(command);
 
-    console.log(`Starting the container '${containerId}' and running tests`);
-    await exec(`docker start ${containerId}`);
+    console.log(`Starting the container '${containerName}' and running tests`);
+    await exec(`docker start ${containerName}`);
 
-    const listTestsCommand = `docker exec submission-${id} find /app/src/test/java -name "*Test.java"`;
+    const listTestsCommand = `docker exec submission-${id} find ${volumePath}/src/test/java -name "*Test.java"`;
     const { stdout: testFiles } = await exec(listTestsCommand);
     const testFilesArray = testFiles.split("\n").filter((file) => file);
 
@@ -34,22 +35,22 @@ export async function runTests(path: string, id: string) {
       throw new Error("No test files found in the directory.");
     }
 
-    const testCommand = `docker exec ${containerId} mvn -f /app/pom.xml test`;
+    const testCommand = `docker exec ${containerName} mvn -f ${volumePath}/pom.xml test`;
 
     console.log(`Running tests with command: ${testCommand}`);
     const { stdout: testResults, stderr: testErrors } = await exec(testCommand);
 
     if (testErrors) {
       console.error(`Test execution errors: ${testErrors}`);
-    }else {
-      console.log("Test Results:\n", testResults);
     }
+
+    console.log("Test Results:\n", testResults);
   } catch (e) {
+    console.error(e);
     throw e;
   } finally {
-    console.log(`Removing the container '${containerId}'`);
-    const removeCommand = `docker rm -f ${containerId}`;
-    await exec(`docker commit ${containerId} my-maven-cached`)
+    console.log(`Removing the container '${containerName}'`);
+    const removeCommand = `docker rm -f ${containerName}`;
     await exec(removeCommand);
   }
 }
