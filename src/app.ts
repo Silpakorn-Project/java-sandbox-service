71323b115a254e4595fc95a809b7e71e4578e2c3
Puriwat <uglypow@gmail.com>
2025-04-18 06:24:03 +0700

    chore: change port

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ const app = createKoaServer({
 
 DotEnv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8002;
 
 app.listen(port, () => {
     const modeMessage =
